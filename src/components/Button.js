073cb42284af4d0aa3c4f8a8fee10de207c4b3ef
Graham Mothersill <graham.mothersill@gmail.com>
2020-07-07 13:13:06 -0600

    add everything so far because I forgot to commit yesterday

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,30 @@
 import React from "react";
-
 import "components/Button.scss";
+var classNames = require('classnames');
+
 
 export default function Button(props) {
-   return <></>;
+   // let buttonClass = "button";
+
+   const buttonClass = classNames("button", {
+      "button--confirm": props.confirm,
+      "button--danger": props.danger
+   });
+
+   // if (props.confirm) {
+   //    buttonClass += " button--confirm";
+   // }
+   // if (props.danger) {
+   //    buttonClass += " button--danger";
+   // }
+
+   return (
+      <button
+         className={buttonClass}
+         onClick={props.onClick}
+         disabled={props.disabled}
+      >
+         {props.children}
+      </button>
+   );
 }
